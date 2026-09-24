7b2032f151f066b22c333cc559913cf9477c4edf
DavidFilmsFs <daas15091810@gmail.com>
2026-09-24 10:58:49 -0600

    Nuevo trabajo

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const name= "David Alejandro";
+//let edad= 18; 
+//edad = 19;//Cambio de edad
+
+const proyectoFavorito= " Lego";
+
+console.log("Mi proyecto favorito es" + proyectoFavorito);
+
+function saludar(nombre) { console.log("Hola +nombre");}
+
+
+let edad = Number(prompt("Ingresa tu edad"));
+
+if( edad >= 18)
+{console.log("Eres mayor de edad");}
+
+else {console.log("Eres menor de edad");}
+
+
+
+
+for(let i= 0; i<=10;i+=2){
+    console.log(i);
+}
